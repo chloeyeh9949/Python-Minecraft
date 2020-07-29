5be110418949a5536a563002507a148c3dae864a
chloeyeh9949 <68846450+chloeyeh9949@users.noreply.github.com>
2020-07-29 16:44:58 +0800

    Add files via upload

---
+++ 3d5.py
@@ -0,0 +1,8 @@
+from mcpi.minecraft import Minecraft
+import time
+mc = Minecraft.create()
+x,y,z = mc.player.getTilePos()
+time.sleep(2)
+mc.setBlocks(x-1,y+3,z-1,x+1,y+5,z+1,161)
+
+mc.setBlocks(x,y,z,x,y+4,z,17)
---
+++ 3d6.py
@@ -0,0 +1,5 @@
+x,y,z = mc.player.getTilePos()
+def Tree(x,y,z):
+    mc.setBlocks(x-1,y+3,z-1,x+1,y+5,z+1,161)
+    mc.setBlocks(x,y,z,x,y+4,z,17)
+Tree(x,y,z)
